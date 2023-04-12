587442f3ff4b3a470dc27ffe21c418db071b2f64
uwulises <ulises.campodonico@ug.uchile.cl>
2023-04-12 18:29:28 -0400

    format document

--- test_pybullet_IK.py
+++ test_pybullet_IK.py
@@ -6,18 +6,20 @@ import numpy as np
 # Initialize PyBullet
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-useFixedBase = True #Para que el brazo no flote o se desplace
-# Load KR6 robot arm and table
-planeId = p.loadURDF("plane.urdf")
-robotId = p.loadURDF("kr6_2.urdf", basePosition=[0, 0, 0],useFixedBase=useFixedBase)
-tableId = p.loadURDF("table/table.urdf", basePosition=[1.5, 0, 0],useFixedBase=useFixedBase)
+useFixedBase = True
+
 # Set gravity and time step
 p.setGravity(0, 0, -9.81)
 p.setTimeStep(1 / 240)
 
-# create sliders for X, Y, Z, A, B, and C
-x_slider = p.addUserDebugParameter("X", -0.5, 2, 1)
-y_slider = p.addUserDebugParameter("Y", -0.5, 0.5, 0)
+# Load KR6 robot arm and table
+planeId = p.loadURDF("plane.urdf")
+robotId = p.loadURDF("kr6_2.urdf", basePosition=[0, 0, 0], useFixedBase = useFixedBase)
+tableId = p.loadURDF("table/table.urdf", basePosition=[1.5, 0, 0], useFixedBase = useFixedBase)
+
+# Create sliders for X, Y, Z, A, B, and C
+x_slider = p.addUserDebugParameter("X", 0, 2, 1)
+y_slider = p.addUserDebugParameter("Y", -1, 1, 0)
 z_slider = p.addUserDebugParameter("Z", 0, 1.5, 1)
 a_slider = p.addUserDebugParameter("A", -3.14, 3.14, 0)
 b_slider = p.addUserDebugParameter("B", -3.14, 3.14, 0)
@@ -33,17 +35,13 @@ while True:
     a = p.readUserDebugParameter(a_slider)
     b = p.readUserDebugParameter(b_slider)
     c = p.readUserDebugParameter(c_slider)
-    #target position
-    xyz=[x,y,z]
-    #target orientation
+    # target position
+    xyz = [x,y,z]
+    # target orientation
     ori = p.getQuaternionFromEuler([a,b,c])
-    #calculate joint target
-    target= p.calculateInverseKinematics(robotId, 5, targetPosition=xyz, targetOrientation=ori)
+    # calculate joint target
+    target = p.calculateInverseKinematics(robotId, 5, targetPosition = xyz, targetOrientation = ori)
     p.stepSimulation()
-    p.setJointMotorControlArray(robotId, range(6), p.POSITION_CONTROL, targetPositions=target)
-    p.stepSimulation()
+    p.setJointMotorControlArray(robotId, range(6), p.POSITION_CONTROL, targetPositions = target)
     time.sleep(0.1)
     
-
-    
-
