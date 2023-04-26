a15e689f077fff77be39aa709f211bfd10f240ce
leslieecv <leslie.cardenas008@gmail.com>
2023-04-26 19:30:44 -0400

    try to solve assignment 1, without success

--- Tareas/tarea_1.py
+++ Tareas/tarea_1.py
@@ -2,8 +2,9 @@ import pybullet as p
 import pybullet_data
 import time
 import numpy as np
-from camera import get_img_rgba, pose_object, detect_borders
 import cv2
+
+
 # Initialize PyBullet
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -20,7 +21,6 @@ robotId = p.loadURDF("brazos/scara_fcfm_model/scara.urdf", basePosition = [0, 0,
 
 """-------- Item 1: Cargar objeto que quieran tomar y ubicar sobre la mesa --------- """
 
-
 """-------- Item 2: Cargar gripper tweezer, ubicar, orientar y restringir 
             correctamente en la punta del efector del SCARA               ---------- """
 
@@ -28,12 +28,20 @@ robotId = p.loadURDF("brazos/scara_fcfm_model/scara.urdf", basePosition = [0, 0,
 n_tcf = 2
 
 # Join gripper to robot
+tweezerId = p.loadURDF("efectores/scara/tweezer/tweezer.urdf", basePosition = [0, 0.6, 0.2])
+
+joint_axis_gripper = [0, 0, 0]
+gripper_parentFramePosition = [0, 0, 0]
+gripper_childFramePosition = [-0.013, 0.107, 0]
+gripper_childFrameOrientation = p.getQuaternionFromEuler([0,0,0])
+joint_constraint = p.createConstraint(robotId, 2, childBodyUniqueId = tweezerId, childLinkIndex= -1, jointType= p.JOINT_FIXED, jointAxis= joint_axis_gripper, parentFramePosition=gripper_parentFramePosition,
+                                     childFramePosition=gripper_childFramePosition, childFrameOrientation=gripper_childFrameOrientation)
 
 
 """ ------  Item 3: Crear una rutina de pick and place, donde el robot tome el
             objeto que eligieron y lo deje en la caja sobre la mesa, mínimo 40 poses. 
             
-            path_SCARA recibe vectores [x, y, z, eff]                       --------- """ 
+            path_SCARA recibe vectores [x, y, z, c, eff]                       --------- """ 
 
 path_SCARA = [[1, 0, 1, 0.01], [1.05, 0, 0, 0]]
 
@@ -41,26 +49,24 @@ path_SCARA = [[1, 0, 1, 0.01], [1.05, 0, 0, 0]]
 rate = 2000
 
 # Run the simulation
+for i in range(len(path_SCARA)):
+    j = 1
+    while (j/rate <= 1):
+
+        # target position
+        xyz = path_SCARA[i][:3]
+        eff = path_SCARA[i][3]
+        
+        # calculate joint target
+        #target = p.calculateInverseKinematics(robotId, endEffectorLinkIndex = n_tcf, targetPosition = xyz)
+        """-------- Item 4: Agregar línea que controla estado del efector ---------"""
+
+        #p.setJointMotorControlArray(robotId, range(n_tcf+1), p.POSITION_CONTROL, targetPositions = target)
+        p.stepSimulation()
+
+        j+=1
 while True:
     p.stepSimulation()
-
-    for i in range(len(path_SCARA)):
-        j = 1
-        while (j/rate <= 1):
-
-            # target position
-            xyz = path_SCARA[i][:3]
-            eff = path_SCARA[i][3]
-            
-            # calculate joint target
-            target = p.calculateInverseKinematics(robotId, endEffectorLinkIndex = n_tcf, targetPosition = xyz)
-            """-------- Item 4: Agregar línea que controla estado del efector ---------"""
-
-            p.setJointMotorControlArray(robotId, range(n_tcf+1), p.POSITION_CONTROL, targetPositions = target)
-            p.stepSimulation()
-
-            j+=1
-
 
 
 
