c28c1524d0ea249894e5f6e6b03e6abde6a6f6c9
uwulises <ulises.campodonico@ug.uchile.cl>
2023-06-15 02:32:17 -0400

    working

--- Tareas/TA3/train_anymal.py
+++ Tareas/TA3/train_anymal.py
@@ -1,57 +1,62 @@
 import pybullet as p
 import pybullet_data
-import time
 import neat
-import random
 import numpy as np
-import gzip
 import pickle
 import csv
+
 # Quadruped simulation parameters
 QUADRUPED_URDF_PATH = "anymal/urdf/anymal.urdf"
-
 LEG_JOINT_NUMBERS = [1, 2, 3, 6, 7, 8, 11, 12, 13, 16, 17, 18]
-NUM_STEPS = 200
+NUM_STEPS = 100
+
 # NEAT parameters
 NUM_INPUTS = 14  # Replace with the number of inputs based on your quadruped's state
 NUM_OUTPUTS = 12  # Each leg has joint control
-up_stairs_x = 4
+MAX_FORCES= [50]*12 # Max force for joints
+up_stairs_x = 4 # Target position
+
 # NEAT training loop
-NUM_GENERATIONS = 30
+NUM_GENERATIONS = 2
 last_distance = 0
 global generation
+
 # Quadruped environment class
-
-
 class QuadrupedEnv:
     def __init__(self):
         p.connect(p.GUI)  # or p.DIRECT for headless mode
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
-        p.loadURDF("plane.urdf")  # Load the ground plane
+        p.loadURDF("plane.urdf") 
         p.loadURDF("objetos/stair.urdf", basePosition = [0, 0, 0], useFixedBase = True)
         p.setGravity(0, 0, -9.81)
-        
-        self.quadruped = p.loadURDF(QUADRUPED_URDF_PATH, basePosition=[0, 0, 0.8])
-        self.joint_ids = LEG_JOINT_NUMBERS
 
-        p.setTimeStep(1 / 400)  # Control the simulation speed
+        p.setTimeStep(1 / 400) 
+
+        self.joint_ids = LEG_JOINT_NUMBERS
+        self.quadruped = p.loadURDF(QUADRUPED_URDF_PATH, basePosition=[0, 0, 0.6])
+        self.log_joints= np.array([])
 
     def hard_reset(self):
+        self.log_joint_values()
+
         p.resetSimulation()
-        p.loadURDF("plane.urdf")  # Load the ground plane
+        p.loadURDF("plane.urdf") 
         p.loadURDF("objetos/stair.urdf", basePosition = [0, 0, 0], useFixedBase = True)
-        self.quadruped = p.loadURDF(QUADRUPED_URDF_PATH, basePosition=[0, 0, 0.8])
         p.setGravity(0, 0, -9.81)
 
+        self.quadruped = p.loadURDF(QUADRUPED_URDF_PATH, basePosition=[0, 0, 0.6])
+        self.log_joints = np.array([])
+
+
     def reset(self):
-        # Reset the simulation
         p.setGravity(0, 0, -9.81)
 
         for index in self.joint_ids:
             p.resetJointState(self.quadruped, jointIndex=index, targetValue=0)
 
     def step(self, joints_state=np.zeros(12)):
-        p.setJointMotorControlArray(self.quadruped, jointIndices=self.joint_ids, controlMode=p.POSITION_CONTROL, targetPositions=joints_state)
+        p.setJointMotorControlArray(self.quadruped, jointIndices=self.joint_ids, controlMode=p.POSITION_CONTROL, targetPositions=joints_state, forces=MAX_FORCES)
+        self.log_joints = np.append(self.log_joints, self.get_quadruped_state())
         p.stepSimulation()
 
     def get_quadruped_state(self):
@@ -71,64 +76,69 @@ class QuadrupedEnv:
 
 
     def get_fitness(self):
-            # Compute and return the fitness score based on the performance of the quadruped
-            # Implement this based on your specific objectives
-            global last_distance
-            fitness = 0.0
-            # TODO
+            
+        # Compute and return the fitness score based on the performance of the quadruped
+        # Implement this based on your specific objectives
 
-            distance_x = p.getLinkState(self.quadruped, 0)[0][0]
-            distance_y = p.getLinkState(self.quadruped, 0)[0][1]
-            distance_z = p.getLinkState(self.quadruped, 0)[0][2]
-            orientation = p.getLinkState(self.quadruped, 0)[1]
-            angle_x = p.getEulerFromQuaternion(orientation)[0]
-            angle_y = p.getEulerFromQuaternion(orientation)[1]
-            angle_z = p.getEulerFromQuaternion(orientation)[2]
+        global last_distance
+        fitness = 0.0
 
+        distance_x = p.getLinkState(self.quadruped, 0)[0][0]
+        distance_y = p.getLinkState(self.quadruped, 0)[0][1]
+        distance_z = p.getLinkState(self.quadruped, 0)[0][2]
+        orientation = p.getLinkState(self.quadruped, 0)[1]
+        angle_x = p.getEulerFromQuaternion(orientation)[0]
+        angle_y = p.getEulerFromQuaternion(orientation)[1]
+        angle_z = p.getEulerFromQuaternion(orientation)[2]
 
+        if (self.check_quadruped()):
+            fitness=-1000
+            last_distance=0.0
 
-            if (self.check_quadruped()):
-                fitness=-1000
-                last_distance=0.0
+        elif (distance_x>last_distance):
+            fitness = distance_x*np.cos(angle_z) - abs(distance_y)
+            last_distance=distance_x
+            
+        elif (round(distance_x,1)==round(last_distance,1)):
+            fitness = -1
+            last_distance=distance_x
 
-            elif (distance_x>last_distance):
-                fitness = distance_x*np.cos(angle_z) - abs(distance_y)
-                last_distance=distance_x
-                
+        elif(round(distance_x)==up_stairs_x):
+            with open('Tareas/TA3/checkpoint/best_genome', 'wb') as f:
+                pickle.dump(best_genome, f)
+            exit()
 
-            elif (round(distance_x,1)==round(last_distance,1)):
-                fitness = -1
-                last_distance=distance_x
+        if abs(angle_z)<0.1:
+            fitness*=0.7
 
-            elif(round(distance_x)==up_stairs_x):
-                with open('Tareas/TA3/checkpoint/best_genome', 'wb') as f:
-                    pickle.dump(best_genome, f)
-            return fitness
+        return fitness
 
     def check_quadruped(self):
         ori = p.getLinkState(self.quadruped, 0)[1]
         angle_x = p.getEulerFromQuaternion(ori)[0]*180/np.pi
         pos_y = p.getLinkState(self.quadruped, 0)[0][1]
+
         if (angle_x < -60 or angle_x > 60):
             quadruped_env.hard_reset()
             return True
+        
         if (abs(pos_y)>1.5):
             quadruped_env.hard_reset()
             return True
+        
         return False
     
-    def log_joint_values(self, log):  
+    def log_joint_values(self):  
 
         with open('Tareas/TA3/joint_log.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(log)
+            writer.writerow(self.log_joints)
+        
 
 # NEAT fitness evaluation
-
-
 def eval_genomes(genomes, config):
-    for genome_id, genome in genomes:
-        quadruped_env.reset()
+    for _, genome in genomes:
+        #quadruped_env.reset()
         quadruped_state = quadruped_env.get_quadruped_state()
         net = neat.nn.FeedForwardNetwork.create(genome, config)
         fitness = 0.0
@@ -139,15 +149,14 @@ def eval_genomes(genomes, config):
             inputs = quadruped_state
             outputs = net.activate(inputs)
             joints_states = outputs[:NUM_OUTPUTS]
+
             quadruped_env.step(joints_states)
             quadruped_state = quadruped_env.get_quadruped_state()
-
             fitness += quadruped_env.get_fitness()
+            
 
         # Assign fitness to the genome
         genome.fitness = fitness
-        if (quadruped_env.check_quadruped()):
-            genome.fitness = -100
 
 
 # NEAT setup
@@ -165,10 +174,10 @@ quadruped_env = QuadrupedEnv()
 
 
 for generation in range(NUM_GENERATIONS):
-    log_joints = np.array([])
+    
     best_genome = population.run(eval_genomes,1)
     # Evaluate the best genome on a final test run
-    quadruped_env.reset()
+    #quadruped_env.reset()
     quadruped_state = quadruped_env.get_quadruped_state()
     best_net = neat.nn.FeedForwardNetwork.create(best_genome, config)
 
@@ -179,12 +188,9 @@ for generation in range(NUM_GENERATIONS):
         joint_angles = outputs[:NUM_OUTPUTS]
         quadruped_env.step(joint_angles)
         quadruped_state = quadruped_env.get_quadruped_state()
-        log_joints = np.append(log_joints,quadruped_state)
-    
-    quadruped_env.log_joint_values(log_joints)
-    quadruped_env.check_quadruped()
+
     # Save the best genome.
     with open('Tareas/TA3/checkpoint/best_genome', 'wb') as f:
         pickle.dump(best_genome, f)
 
-
+quadruped_env.log_joint_values()
