a7bfbb14cfef6bab02ddf0d485e5384b98429a8d
uwulises <ulises.campodonico@ug.uchile.cl>
2023-08-31 00:54:24 -0400

    create file to test environment

---
+++ herramientas/check_environment.py
@@ -0,0 +1,14 @@
+import numpy
+import pandas
+import scipy
+import matplotlib
+import pyserial
+import cv2
+import pygame
+import websockets
+import djitellopy
+import pybullet
+import pybullet_data
+
+print('Todas las librearias estan instaladas correctamente')
+print('Welcome to ME5150')
