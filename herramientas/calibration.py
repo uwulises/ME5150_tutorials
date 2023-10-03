1ff779927f0b0c264c1bf991418267c18ad29330
leslieecv <leslie.cardenas008@gmail.com>
2023-10-03 18:38:16 -0300

    change libraries and add calibration file

--- herramientas/calibration.py
+++ herramientas/calibration.py
@@ -1,56 +1,92 @@
-import numpy as np
+# Import required modules
 import cv2
-import yaml
-
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
+import numpy as np
+import os
+import glob
+  
+# Define the dimensions of checkerboard
+CHECKERBOARD = (6, 9)
+  
+# stop the iteration when specified
+# accuracy, epsilon, is reached or
+# specified number of iterations are completed.
+criteria = (cv2.TERM_CRITERIA_EPS + 
+            cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+  
+  
+threedpoints = []
+twodpoints = []  
+#  3D points real world coordinates
+objectp3d = np.zeros((1, CHECKERBOARD[0] 
+                      * CHECKERBOARD[1], 
+                      3), np.float32)
+objectp3d[0, :, :2] = np.mgrid[0:CHECKERBOARD[0],
+                               0:CHECKERBOARD[1]].T.reshape(-1, 2)*26
+prev_img_shape = None
+  
+# Extracting path of individual image stored
+# in a given directory. Since no path is
+# specified, it will take current directory
+# jpg files alone
 cap = cv2.VideoCapture(1)
 found = 0
-while(found < 10):  # Here, 10 can be changed to whatever number you like to choose
-    ret, img = cap.read() # Capture frame-by-frame
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+while(found < 50):  # Here, 10 can be changed to whatever number you like to choose
+    ret, image = cap.read() # Capture frame-by-frame
+    grayColor = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  
     # Find the chess board corners
-    ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-
-    # If found, add object points, image points (after refining them)
+    # If desired number of corners are
+    # found in the image then ret = true
+    ret, corners = cv2.findChessboardCorners(
+                    grayColor, CHECKERBOARD, 
+                    cv2.CALIB_CB_ADAPTIVE_THRESH 
+                    + cv2.CALIB_CB_FAST_CHECK + 
+                    cv2.CALIB_CB_NORMALIZE_IMAGE)
+  
+    # If desired number of corners can be detected then,
+    # refine the pixel coordinates and display
+    # them on the images of checker board
     if ret == True:
-        objpoints.append(objp)   # Certainly, every loop objp is the same, in 3D.
-        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-        imgpoints.append(corners2)
-
+        threedpoints.append(objectp3d)
+  
+        # Refining pixel coordinates
+        # for given 2d points.
+        corners2 = cv2.cornerSubPix(
+            grayColor, corners, (11, 11), (-1, -1), criteria)
+  
+        twodpoints.append(corners2)
+  
         # Draw and display the corners
-        img = cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+        image = cv2.drawChessboardCorners(image, 
+                                          CHECKERBOARD, 
+                                          corners2, ret)
         found += 1
-
-    cv2.imshow('img', img)
+    cv2.imshow('img', image)
     cv2.waitKey(10)
-
-# When everything done, release the capture
-cap.release()
+  
 cv2.destroyAllWindows()
-
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
-# It's very important to transform the matrix to list.
-
-data = {'camera_matrix': np.asarray(mtx).tolist(), 'dist_coeff': np.asarray(dist).tolist()}
-
-with open("calibration.yaml", "w") as f:
-    yaml.dump(data, f)
-
-"""
-with open('calibration.yaml') as f:
-    loadeddict = yaml.load(f)
-
-mtxloaded = loadeddict.get('camera_matrix')
-distloaded = loadeddict.get('dist_coeff')"""
+  
+h, w = image.shape[:2]
+  
+  
+# Perform camera calibration by
+# passing the value of above found out 3D points (threedpoints)
+# and its corresponding pixel coordinates of the
+# detected corners (twodpoints)
+ret, matrix, distortion, r_vecs, t_vecs = cv2.calibrateCamera(
+    threedpoints, twodpoints, grayColor.shape[::-1], None, None)
+  
+  
+# Displaying required output
+print(" Camera matrix:")
+print(np.array(matrix, dtype=np.int32))
+  
+print("\n Distortion coefficient:")
+print(np.array(distortion, dtype=np.float16))
+  
+#print("\n Rotation Vectors:")
+#print(r_vecs)
+  
+#print("\n Translation Vectors:")
+#print(t_vecs)
