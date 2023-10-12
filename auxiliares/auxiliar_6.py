249b7b1c4ecbb2496f3a68f833aa20f03033b29f
leslieecv <leslie.cardenas008@gmail.com>
2023-10-12 16:18:06 -0300

    update aux6

--- auxiliares/auxiliar_6.py
+++ auxiliares/auxiliar_6.py
@@ -1,7 +1,13 @@
+import os
+import sys
 import pybullet as p
 import pybullet_data
 import numpy as np
 import cv2
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../herramientas')))
+
+from aruco import ArucoHunting
+from shape_detector import ShapeDetector
 
 # Initialize PyBullet
 physicsClient = p.connect(p.GUI)
@@ -16,83 +22,61 @@ cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 cube = p.loadURDF("modelos/objetos/cubo.urdf", basePosition=cubeStartPos, useFixedBase=True)
 
-
 def traslation_transform(x, y, z):
     return np.array([[1, 0, 0, x],
                      [0, 1, 0, y],
                      [0, 0, 1, z],
                      [0, 0, 0, 1]])
 
-def get_aruco_pose(frame):
-        # Parametros cámara
-        camera_matrix = np.array([[0., 0., 0.],[0., 0., 0.],[0., 0., 1.]])
-        dist_coeff = np.array([[0.,  0., 0.,  0., 0.]])
-
-        # Parametros marcador
-        _marker_length = 0.028 # Ajustar a la medida real del marcador
-        _aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250) # Diccionario de marcadores
-
-        _draw_img = np.copy(frame)
-        _frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # Carga parametros de detección
-        _aruco_parameters = cv2.aruco.DetectorParameters()
-        # Crea detector a partir del diccionario y los parametros
-        _detector = cv2.aruco.ArucoDetector(_aruco_dict, _aruco_parameters)
-
-        # Detecta marcadores
-        marker_corners, _, _ = _detector.detectMarkers(_frame_gray)
-
-        if marker_corners == ():
-            return None
-        # Crea distribución de puntos 3D de cualquier marcador (es igual para todos)
-        _objPoints = np.array([[-_marker_length/2, _marker_length/2, 0], [_marker_length/2, _marker_length/2, 0],
-                               [_marker_length/2, -_marker_length/2, 0], [-_marker_length/2, -_marker_length/2, 0]])
-        
-        for _marker_corner in marker_corners:
-            # Calcula pose de cada marcador detectado
-            _valid, rvec, tvec = cv2.solvePnP(_objPoints, _marker_corner, camera_matrix, dist_coeff, flags=cv2.SOLVEPNP_IPPE_SQUARE)
-            # Dibuja ejes de cada marcador detectado
-            _draw_img = cv2.drawFrameAxes(_draw_img, camera_matrix, dist_coeff, rvec, tvec, _marker_length)
-
-        # Se ajustan ejes de coordenadas
-        rvec = np.transpose(rvec)
-        tvec = np.transpose(tvec)[0]
-
-        # Dibuja marcadores detectados sobre imagen original
-        _draw_img = cv2.aruco.drawDetectedMarkers(_draw_img, marker_corners, borderColor=(255, 0, 0))
-        _draw_img = cv2.cvtColor(_draw_img, cv2.COLOR_BGR2RGB)
-        #cv2.imshow('frame', _draw_img)
-
-        # Retorna pose de marcador detectado
-        pose_aruco = np.array([tvec, rvec[0]])
-        return pose_aruco
-
 def perm_columns(pos):
+    # TODO -------- Parte 2.c: Permutar valores ejes coordenados para ajustar a coordenadas PyBullet
     n_position = np.zeros(3)
-    n_position[0] = pos[2] 
-    n_position[1] = pos[0]
-    n_position[2] = -pos[1]
-    return n_position
+    return pos # Cambiar por n_position para ver el efecto
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
+
+camera_matrix = np.array([[1080., 0., 290.],[0., 1072., 250.],[0., 0., 1.]])
+dist_coeff = np.array([[-1.125,  7.71, -0.044,  0.0143, -4.105]])
+
+hunter = ArucoHunting()
+# TODO -------- Parte 2.b: Cambiar el valor del largo del marcador
+hunter.set_marker_length(0.05) # en metros
+# TODO -------- Parte 2.c: Cambiar los valores de la matriz de la cámara y coeficientes de distorsión
+hunter.set_camera_parameters(camera_matrix, dist_coeff)
+
+shapito = ShapeDetector()
+
+flagP2 = False
+flagP3 = True
 
 while cap.isOpened():
     ret, frame = cap.read()
+    hunter.update_image(frame)
+    hunter.update_pose_and_corners()
 
-    pose = get_aruco_pose(frame)
+    if flagP2:
+        aruco_pose = hunter.pose
 
-    if pose is not None:
-        # Permutar columnas para ajustar a PyBullet
-        position = perm_columns(pose[0])
-        # Convertir a cuaternión
-        orientation = p.getQuaternionFromEuler(pose[1])
-        # Actualizar posición y orientación del cubo
-        p.resetBasePositionAndOrientation(cube, position, orientation)
+        if aruco_pose is not None:
+            # Permutar ejes coordenados para ajustar a PyBullet
+            position = perm_columns(aruco_pose[0])
+            # Convertir a cuaternión
+            orientation = p.getQuaternionFromEuler(aruco_pose[1])
+            # Actualizar posición y orientación del cubo
+            p.resetBasePositionAndOrientation(cube, position, orientation)
+
         p.stepSimulation()
+        cv2.imshow('ArUco detection', hunter.img_detection)
 
-    p.stepSimulation()
-    cv2.imshow('frame', frame)
+    if flagP3:
+        shapito.update_image(frame)
+        x, y = shapito.find_rectangle()
+        #print(x, y)
+        
+        aruco_corners = hunter.corner
+
+        cv2.imshow('Rectangulos', shapito.img_detection)
+        
 
     if cv2.waitKey(1) == ord('q'):
         break
