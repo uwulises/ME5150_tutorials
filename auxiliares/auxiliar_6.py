f1e4e395d42b110c57b72cb9953810b32b866a20
leslieecv <leslie.cardenas008@gmail.com>
2023-10-11 23:48:26 -0300

    add comments to aux 6

--- auxiliares/auxiliar_6.py
+++ auxiliares/auxiliar_6.py
@@ -9,11 +9,9 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 # Set gravity and time step
 p.setGravity(0, 0, -9.81)
-p.setTimeStep(1 / 240) # Para el dron se recomienda trabajar con stepSimulation
-p.setRealTimeSimulation(1) # 0 Desactiva la simulacion en tiempo real
+p.setTimeStep(1 / 240)
+p.setRealTimeSimulation(1)
 
-# Cargar ambiente de simulacion
-#planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0, 0, 1]
 cubeStartOrientation = p.getQuaternionFromEuler([0, 0, 0])
 cube = p.loadURDF("modelos/objetos/cubo.urdf", basePosition=cubeStartPos, useFixedBase=True)
@@ -26,34 +24,47 @@ def traslation_transform(x, y, z):
                      [0, 0, 0, 1]])
 
 def get_aruco_pose(frame):
-        # Parametros camara
-        camera_matrix = np.array([[1080., 0., 290.],[0., 1072., 250.],[0., 0., 1.]])
-        dist_coeff = np.array([[-1.125,  7.71, -0.044,  0.0143, -4.105]])
+        # Parametros cámara
+        camera_matrix = np.array([[0., 0., 0.],[0., 0., 0.],[0., 0., 1.]])
+        dist_coeff = np.array([[0.,  0., 0.,  0., 0.]])
+
+        # Parametros marcador
+        _marker_length = 0.028 # Ajustar a la medida real del marcador
+        _aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250) # Diccionario de marcadores
 
         _draw_img = np.copy(frame)
         _frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+        
+        # Carga parametros de detección
         _aruco_parameters = cv2.aruco.DetectorParameters()
+        # Crea detector a partir del diccionario y los parametros
         _detector = cv2.aruco.ArucoDetector(_aruco_dict, _aruco_parameters)
 
+        # Detecta marcadores
         marker_corners, _, _ = _detector.detectMarkers(_frame_gray)
 
         if marker_corners == ():
             return None
-        
-        _marker_length = 0.028 # Ajustar a la medida real
-
+        # Crea distribución de puntos 3D de cualquier marcador (es igual para todos)
         _objPoints = np.array([[-_marker_length/2, _marker_length/2, 0], [_marker_length/2, _marker_length/2, 0],
                                [_marker_length/2, -_marker_length/2, 0], [-_marker_length/2, -_marker_length/2, 0]])
         
         for _marker_corner in marker_corners:
+            # Calcula pose de cada marcador detectado
             _valid, rvec, tvec = cv2.solvePnP(_objPoints, _marker_corner, camera_matrix, dist_coeff, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+            # Dibuja ejes de cada marcador detectado
             _draw_img = cv2.drawFrameAxes(_draw_img, camera_matrix, dist_coeff, rvec, tvec, _marker_length)
 
+        # Se ajustan ejes de coordenadas
         rvec = np.transpose(rvec)
         tvec = np.transpose(tvec)[0]
+
+        # Dibuja marcadores detectados sobre imagen original
         _draw_img = cv2.aruco.drawDetectedMarkers(_draw_img, marker_corners, borderColor=(255, 0, 0))
         _draw_img = cv2.cvtColor(_draw_img, cv2.COLOR_BGR2RGB)
+        #cv2.imshow('frame', _draw_img)
+
+        # Retorna pose de marcador detectado
         pose_aruco = np.array([tvec, rvec[0]])
         return pose_aruco
 
@@ -72,16 +83,16 @@ while cap.isOpened():
     pose = get_aruco_pose(frame)
 
     if pose is not None:
-        pos = perm_columns(pose[0])
-        q = p.getQuaternionFromEuler(pose[1])
-        print(np.array(pos, dtype = np.float16))
-        p.resetBasePositionAndOrientation(cube, pos, q)
+        # Permutar columnas para ajustar a PyBullet
+        position = perm_columns(pose[0])
+        # Convertir a cuaternión
+        orientation = p.getQuaternionFromEuler(pose[1])
+        # Actualizar posición y orientación del cubo
+        p.resetBasePositionAndOrientation(cube, position, orientation)
         p.stepSimulation()
-    #p.setLinkState(cube, 0, pose[:3], pose[3:])
 
     p.stepSimulation()
     cv2.imshow('frame', frame)
-
 
     if cv2.waitKey(1) == ord('q'):
         break
