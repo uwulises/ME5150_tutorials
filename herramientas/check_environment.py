d96a4cba2ae8bcd6040d647ad4146fc49d32f282
Leslie Cárdenas <51496611+leslieecv@users.noreply.github.com>
2024-04-03 18:22:14 -0300

    Update check_environment.py

--- herramientas/check_environment.py
+++ herramientas/check_environment.py
@@ -2,12 +2,12 @@ import numpy
 import pandas
 import scipy
 import matplotlib
-import pyserial
+import serial
 import cv2
 import pygame
 import websockets
 import pybullet
 import pybullet_data
 
-print('Todas las librearias estan instaladas correctamente')
-print('Welcome to ME5150')
+print('Todas las librerias estan instaladas correctamente')
+print('------------Welcome to ME5150------------')
