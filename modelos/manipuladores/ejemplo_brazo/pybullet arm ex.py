ba35c50da2812fa8a1abe4f9181d8134063ac3b7
Karen Quezada <karen.soledad.quezada@gmail.com>
2024-04-05 00:12:01 -0300

    Added kuka_actividad

--- modelos/manipuladores/ejemplo_brazo/pybullet arm ex.py
+++ modelos/manipuladores/ejemplo_brazo/pybullet arm ex.py
@@ -6,6 +6,8 @@ import numpy as np
 # Connect to the PyBullet simulator
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) # Set the search path to find the plane.urdf file
+
+
 p.setGravity(0,0,-9.81) # Set the gravity to be in the negative z direction
 p.setRealTimeSimulation(1) # Set the simulation to be real time
 
